c6b08e7370a99941dafd3831043d6f26a6784227
John <john.brockman@academytrainee.com>
2019-08-20 14:03:28 +0100

    AppTest Update; Added blackJackTest method in test file

--- src/test/java/com/qa/AppTest.java
+++ src/test/java/com/qa/AppTest.java
@@ -1,5 +1,6 @@
 package com.qa;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -11,12 +12,22 @@ public class AppTest
 {
     private App app;
 
+
+
     @Test
     // Tests always neetd to be public, tests should always be void
     public void mainTest(){
         app = new App();
         String[] input = new String[]{"a"};
         app.main( input);
+    }
+
+    @Test
+    public void blackJackTest(){
+        assertEquals(app.blackJack(10, 21), 21);
+        assertEquals(app.blackJack(20, 18), 20);
+        assertEquals(app.blackJack(1, 22), 1);
+        assertEquals(app.blackJack(22, 23), 0);
     }
 
 
