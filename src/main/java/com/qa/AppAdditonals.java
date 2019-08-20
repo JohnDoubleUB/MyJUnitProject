4f332133b66bed5c147ac804171aae348741a16d
John <john.brockman@academytrainee.com>
2019-08-20 16:49:45 +0100

    Ready to add logic to sortDesc! 2

--- src/main/java/com/qa/AppAdditonals.java
+++ src/main/java/com/qa/AppAdditonals.java
@@ -1,7 +1,28 @@
 package com.qa;
 
+import java.util.HashMap;
+
 public class AppAdditonals {
+
+    //sortDesc(123) -> 321
+    //sortDesc(1254859723) -> 9875543221
+    //sortDesc(73065) -> 76530
+
     public int sortDesc(int number){
+
+        String result = "";
+
+        //HashMap<String, Integer> stringToInt = new HashMap<String, Integer>();
+
+
+        String numbers = Integer.toString(number);
+        int numberLength = numbers.length();
+
+        // Get
+
+        for(int i = 0; i < numbers.length(); i++){
+
+        }
 
 
         return 0;
